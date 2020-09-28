31340f900f6f9702e8bd17223516132185e60703
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-09-28 15:39:00 -0500

    feat: completed question 2.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         int myFavoriteNumber = 3;
         System.out.println(myFavoriteNumber);
+        String myString = "Howdy, World!";
+        System.out.println(myString);
     }
 }
