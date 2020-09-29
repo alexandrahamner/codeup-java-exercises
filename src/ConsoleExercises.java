b3b92686405b7dd22a3d86d312cb5022ce96b06e
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-09-29 13:54:21 -0500

    feat: completed first question of the scanner exercise.

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class ConsoleExercises {
     public static void main(String[] args) {
         double pi = 3.14159;
-        System.out.printf("The value of pi is approximately %.2f", pi);
+        System.out.printf("The value of pi is approximately %.2f\n", pi);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your favorite number?");
+        int userNumber = sc.nextInt();
+        System.out.printf("Your number was %d.", userNumber);
     }
 }
