3c27a3c11247ae831db81a4f77cfda144b525a60
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-09-29 14:13:51 -0500

    feat: completed the third question of the scanner exercise.

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -10,13 +10,19 @@ public class ConsoleExercises {
 //        System.out.printf("Your number was %d.", userNumber);
 //        When you input a response that is not an int, you receive the error message: "Exception in thread "main" java.util.InputMismatchException"
 
-        System.out.println("Quick! Give me 3 random words!");
-        String firstWord = sc.next();
-        String secondWord = sc.next();
-        String thirdWord = sc.next();
-        System.out.printf("The 3 words you came up with were: %s, %s, and %s", firstWord, secondWord, thirdWord);
+//        System.out.println("Quick! Give me 3 random words!");
+//        String firstWord = sc.next();
+//        String secondWord = sc.next();
+//        String thirdWord = sc.next();
+//        System.out.printf("The 3 words you came up with were: %s, %s, and %s\n", firstWord, secondWord, thirdWord);
 //        If you enter more than three words, only the first 3 words are taken in.
 //        If you enter less than three words, it waits until you give a third word.
+
+//        You cannot have other sc.next(); before a sc.nextLine(); it doesn't give the user space to type in their response.
+        System.out.println("Give me your favorite quote.");
+        String quote = sc.nextLine();
+        System.out.printf("\"%s\" is a good quote!", quote);
+
 
     }
 }
