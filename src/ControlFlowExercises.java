8ab1bf377168feaa4682401bee76f922e56c19a3
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-10-01 11:00:12 -0500

    feat: do-while loop exercise, counting by 2s from 0 to 100.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,12 +1,15 @@
 public class ControlFlowExercises {
 
     public static void main(String[] args) {
-        int i = 5;
-        while (i <= 15) {
-            System.out.print(i + " ");
-            i++;
-        }
+        int i = 0;
+//        while (i <= 15) {
+//            System.out.print(i + " ");
+//            i++;
+//        }
 
-
+        do {
+            System.out.print(i + "\n");
+            i += 2;
+        } while (i <= 100);
     }
 }
