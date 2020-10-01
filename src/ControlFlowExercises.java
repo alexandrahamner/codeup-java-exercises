17b6cee3320a679376e3c0e81279abf2bc3dbc20
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-10-01 11:07:34 -0500

    feat: do-while loop exercise, counting backwards by 5s from 100 to -10.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,15 +1,20 @@
 public class ControlFlowExercises {
 
     public static void main(String[] args) {
-        int i = 0;
+        int i = 100;
 //        while (i <= 15) {
 //            System.out.print(i + " ");
 //            i++;
 //        }
 
+//        do {
+//            System.out.print(i + "\n");
+//            i += 2;
+//        } while (i <= 100);
+
         do {
             System.out.print(i + "\n");
-            i += 2;
-        } while (i <= 100);
+            i -= 5;
+        } while (i >= -10);
     }
 }
