4b46330753dd69dd9db1412cad89746253776fe2
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-10-01 14:20:17 -0500

    feat: completed the grade exercise.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -59,14 +59,11 @@ public class ControlFlowExercises {
         for (int x = 1; x <= 100; x++) {
             if (((x % 5) == 0) && ((x % 3) == 0)) {
                 System.out.println("FizzBuzz");
-            }
-            else if (x % 5 == 0) {
+            } else if (x % 5 == 0) {
                 System.out.println("Buzz");
-            }
-            else if (x % 3 == 0) {
+            } else if (x % 3 == 0) {
                 System.out.println("Fizz");
-            }
-            else {
+            } else {
                 System.out.println(x);
 
             }
@@ -83,21 +80,51 @@ public class ControlFlowExercises {
         Scanner sc = new Scanner(System.in);
         System.out.println("Do you want to enter a number? [y/n]");
         String userAnswer = sc.next();
+//
+//        if(userAnswer.equals("y")) {
+//            boolean leftJustifiedRows = false;
+//            System.out.println("What number would you like to go up to?");
+//            int userNumber = sc.nextInt();
+//            System.out.println("Number | Squared | Cubed");
+//            System.out.println("------ | ------- | -----");
+//            for(int i = 0; i <= userNumber; i++) {
+//                System.out.println("  " + i + "    |    " + (i * i) + "    |   " + (i * i * i));
+//            }
+//        }
 
-        if(userAnswer.equals("y")) {
-            boolean leftJustifiedRows = false;
-            System.out.println("What number would you like to go up to?");
-            int userNumber = sc.nextInt();
-            System.out.println("Number | Squared | Cubed");
-            System.out.println("------ | ------- | -----");
-            for(int i = 0; i <= userNumber; i++) {
-                System.out.println("  " + i + "    |    " + (i * i) + "    |   " + (i * i * i));
+/*
+    Convert given number grades into letter grades.
+
+    Prompt the user for a numerical grade from 0 to 100.
+    Display the corresponding letter grade.
+    Prompt the user to continue.
+    Assume that the user will enter valid integers for the grades.
+    The application should only continue if the user agrees to.
+    Grade Ranges:
+
+    A : 100 - 88
+    B : 87 - 80
+    C : 79 - 67
+    D : 66 - 60
+    F : 59 - 0
+ */
+
+        if (userAnswer.equals("y")) {
+            System.out.println("Enter your grade for the semester.");
+            int userGrade = sc.nextInt();
+
+            if (userGrade <= 59) {
+                System.out.println("Your grade is a F. Forget about it.");
+            } else if ((userGrade >= 60) && (userGrade <= 66)) {
+                System.out.println("Your grade is a D. Do better.");
+            } else if ((userGrade >= 67) && (userGrade <= 79)) {
+                System.out.println("Your grade is a C. Come on, is that the best you can do?");
+            } else if ((userGrade >= 80) && (userGrade <= 87)) {
+                System.out.println("Your grade is a B. Best grade you've gotten so far.");
+            } else if ((userGrade >= 88) && (userGrade <= 100)) {
+                System.out.println("Your grade is an A. Awesome!");
             }
+
         }
-
-
-
-
-
     }
 }
