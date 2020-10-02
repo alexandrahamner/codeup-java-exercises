eb78bd4c89601e5dcc97d420b82abad6848a9a8a
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-10-02 16:24:54 -0500

    feat: completed the getInteger exercise.

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class MethodsExercises {
     public static void main(String[] args) {
 //Basic Arithmetic
@@ -6,6 +8,9 @@ public class MethodsExercises {
         System.out.println( multiplication(4, 5) );
         System.out.println( division(4, 0) );
         System.out.println( modulus(4, 5) );
+
+//Create a method that validates that user input is in a certain range
+        System.out.println( getInteger(1, 10) );
     }
 
 //Basic Arithmetic
@@ -29,7 +34,24 @@ public class MethodsExercises {
         return a % b;
     }
 
-//Food for thought: What happens if we try to divide by zero? What should happen?
-//Your return is Infinity but it should be zero, because any number divided by zero is zero.
+/*
+Food for thought: What happens if we try to divide by zero? What should happen?
+Your return is Infinity but it should be zero, because any number divided by zero is zero.
+*/
+
+//Create a method that validates that user input is in a certain range
+    public static int getInteger(int min, int max) {
+        System.out.printf("Give me a number between %d and %d\n", min, max);
+        Scanner sc = new Scanner(System.in);
+        int userInput = sc.nextInt();
+
+        if((userInput < min) || (userInput > max)) {
+            System.out.println("You did it wrong! Try again.");
+            return getInteger(min, max);
+        }
+        System.out.printf("Great choice, %d is a good number!\n", userInput);
+        return userInput;
+    }
+
 
 }
