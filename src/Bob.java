46599b18bdecda137b6045fa27eeb7d31c2f9ed4
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-10-02 11:19:12 -0500

    feat: completed the bob exercise.

---
+++ src/Bob.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean userConfirmation = true;
+
+        System.out.println("Hey, I'm Bob. What's up? If you're done talking to me, just say \"Convo is over.\"");
+
+        do {
+            String userResponse = sc.nextLine();
+
+            if (userResponse.endsWith("?")) {
+                System.out.println("Sure.");
+            } else if (userResponse.endsWith("!")) {
+                System.out.println("Whoa, chill out bro!");
+            } else if (userResponse.trim().isEmpty()) {
+                System.out.println("Fine, be that way.");
+            } else if (userResponse.equalsIgnoreCase("Convo is over.")) {
+                System.out.println("I didn't want to talk anyways.");
+                userConfirmation = false;
+            } else {
+                System.out.println("Whatever.");
+            }
+        } while(userConfirmation);
+
+    }
+}
