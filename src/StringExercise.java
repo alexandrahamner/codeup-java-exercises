51a6a18c071ccd45a16a40ae1cd3f8c0da8da491
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-10-02 10:22:17 -0500

    feat: completed the first string basics statement.

---
+++ src/StringExercise.java
@@ -0,0 +1,8 @@
+public class StringExercise {
+    public static void main(String[] args) {
+//Strings Basics 1.
+        String firstStatement = "We don't need no education\n" +
+                "We don't need no thought control ";
+        System.out.println(firstStatement);
+    }
+}
