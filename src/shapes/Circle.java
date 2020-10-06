b9f1d93135ab90f3ff770b751402c0755d7ce7a5
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-10-06 10:19:36 -0500

    feat: completed the circles exercise for oop lecture.

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -5,14 +5,15 @@ public class Circle {
     private double radius;
 
     public Circle(double radius) {
-
+        this.radius = radius;
     }
 
-//    public double getArea() {
-//        return Math.PI * (radius ^ 2);
-//    }
-//    public double getCircumference() {
-//        return 2 * Math.PI * radius;
-//    }
+    public double getArea(){
+        return Math.PI * (this.radius * this.radius);
+    }
+
+    public double getCircumference() {
+        return 2 * Math.PI * this.radius;
+    }
 
 }
