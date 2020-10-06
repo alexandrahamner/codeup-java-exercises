55acd03c3b98a11fc18da110dd86c52573b9ccf1
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-10-06 07:47:46 -0500

    feat: working on the circles exercise.

---
+++ src/shapes/Circle.java
@@ -0,0 +1,16 @@
+package shapes;
+
+public class Circle {
+
+    public Circle(double radius) {
+
+    }
+
+//    public double getArea() {
+//        return Math.PI * (radius ^ 2);
+//    }
+//    public double getCircumference() {
+//        return 2 * Math.PI * radius;
+//    }
+
+}
