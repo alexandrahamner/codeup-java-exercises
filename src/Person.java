c5e1ad5da53a8787b60ca73a533454dadb46cf0f
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-10-06 09:50:03 -0500

    feat: added comments and a constructor for the using reference assignment.

--- src/Person.java
+++ src/Person.java
@@ -2,6 +2,11 @@ public class Person {
 //Object basics
     private String name;
 
+//Made a constructor in order for the "using references" to work.
+    public Person(String personName) {
+        this.name = personName;
+    }
+
     public String getName(){
         return name;
     }
@@ -15,10 +20,32 @@ public class Person {
     }
 
     public static void main(String[] args) {
-        Person person1 = new Person();
-        person1.setName("Bob");
+//        Person person1 = new Person();
+//        person1.setName("Bob");
+//        System.out.println(person1.getName());
+//        person1.sayHello();
+
+//The first SOUT is true because the strings are equal, but the second SOUT is false because they are two different objects with different IDs.
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+
+//I initially thought it would be false, since it is two different objects. I am guessing you are just making an exact duplicate of person1 and assigning it to person2
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+
+//It is going to set both person1 and person2 names as Jane because they are duplicates of each other.
+        Person person1 = new Person("John");
+        Person person2 = person1;
         System.out.println(person1.getName());
-        person1.sayHello();
+        System.out.println(person2.getName());
+        person2.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
     }
+
+
 
 }
