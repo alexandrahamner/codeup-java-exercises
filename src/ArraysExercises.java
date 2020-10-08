ece3ab34641399d9525e9cafcc0b9650e576dd54
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-10-08 09:32:33 -0500

    feat: notes from walkthrough and made a few changes to the code.

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -19,17 +19,23 @@ public class ArraysExercises {
         }
 
         //Testing the addPerson method.
-        addPerson(persons, new Person("Andrew"));
-    }
-
-    public static void addPerson(Person[] arr, Person individualToAdd){
-        //Makes a copy of a Person array and makes it one more person longer.
-        Person[] newPersonArray = Arrays.copyOf(arr, arr.length + 1);
-        //Puts the individual you need to add to the last index in the copied array.
-        newPersonArray[newPersonArray.length - 1] = individualToAdd;
-        //Loops through the array and prints out each individual from the copied Array, including the new individual.
-        for(Person individual : newPersonArray) {
+        Person [] addedPersonArr = addPerson(persons, new Person("Andrew"));
+        System.out.println();
+        for(Person individual : addedPersonArr) {
             System.out.println(individual.getName());
         }
+    }
+
+//    Changed the return type from void to Person[] in order to return the new array.
+    public static Person[] addPerson(Person[] arr, Person individualToAdd){
+        //Makes a copy of a Person array and makes it one more person longer.
+        arr = Arrays.copyOf(arr, arr.length + 1);
+        //Puts the individual you need to add to the last index in the copied array.
+        arr[arr.length - 1] = individualToAdd;
+        //Loops through the array and prints out each individual from the copied Array, including the new individual.
+//        for(Person individual : newPersonArray) {
+//            System.out.println(individual.getName());
+//        }
+        return arr;
     }
 }
