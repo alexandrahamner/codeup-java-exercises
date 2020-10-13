78504ba693409845f7fcd51b784396235474cb06
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-10-13 12:30:03 -0500

    feat: changed the ArrayList because it was not working with the gradesApplication. The student constructor adds a new empty ArrayList when a new student is created. Added a getGrades method.

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -5,10 +5,11 @@ import java.util.ArrayList;
 public class Student {
     // Properties
     private final String name;
-    private static ArrayList<Integer> grades = new ArrayList<>();
+    private static ArrayList<Integer> grades;
 
+    //Testing to make sure methods work! (They work.)
     public static void main(String[] args) {
-        Student luna = new Student("Luna", grades);
+        Student luna = new Student("Luna");
         System.out.println(luna.getName());
         addGrade(87);
         addGrade(90);
@@ -18,20 +19,24 @@ public class Student {
 
     }
     // Constructor to create a student
-    public Student(String name, ArrayList<Integer> grades){
+    public Student(String name){
         this.name = name;
-        this.grades = grades;
+        this.grades = new ArrayList<>();
     }
 
     // returns the student's name
     public String getName() {
         return this.name;
     }
+
+    public ArrayList<Integer> getGrades(){
+        return this.grades;
+    }
     // adds the given grade to the grades property
     public static void addGrade(int grade) {
         grades.add(grade);
     }
-    // returns the average of the students grades
+    // returns the average of the students grades, correctly calculates the average/mean.
     public static double getGradeAverage(ArrayList<Integer> grades) {
         Integer sum = 0;
         if(!grades.isEmpty()) {
