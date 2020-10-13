f6ebea458588c20dfa980107db3e803b1c440237
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-10-13 13:54:22 -0500

    feat: completed the second part of the student exercise; created a hashmap and added 4 students to it.

---
+++ src/grades/GradesApplication.java
@@ -0,0 +1,55 @@
+package grades;
+import java.util.HashMap;
+import static grades.Student.getGradeAverage;
+
+public class GradesApplication {
+
+
+    public static void main(String[] args) {
+        HashMap<String, Student> students = new HashMap<>();
+
+//      Student Variables
+        Student luna = new Student("Luna");
+        Student andrew = new Student("Andrew");
+        Student emily = new Student("Emily");
+        Student liz = new Student("Liz");
+
+        // Add grades to each student
+        // Luna
+        luna.addGrade(87);
+        luna.addGrade(90);
+        luna.addGrade(75);
+        System.out.println("Luna's Grades: " + getGradeAverage(luna.getGrades()));
+
+        // Andrew
+        andrew.addGrade(96);
+        andrew.addGrade(92);
+        andrew.addGrade(85);
+        System.out.println("Andrew's Grades: " + getGradeAverage(andrew.getGrades()));
+
+        //Emily
+        emily.addGrade(82);
+        emily.addGrade(87);
+        emily.addGrade(85);
+        System.out.println("Emily's Grades: " + getGradeAverage(emily.getGrades()));
+
+        //Liz
+        liz.addGrade(72);
+        liz.addGrade(84);
+        liz.addGrade(90);
+        System.out.println("Liz's Grades: " + getGradeAverage(liz.getGrades()));
+
+        //Add Students to HashMap
+        students.put("lunatic-15", luna);
+        students.put("andrewC85", andrew);
+        students.put("emmy-90", emily);
+        students.put("lizp1970", liz);
+
+
+
+
+
+
+
+    }
+}
